29207f7dd80bbe40e3beb4d3dd9c4feee3e2e832
AYABE <ayabe@rafael.co.il>
2026-01-01 15:25:09 +0200

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import moviesRoutes from "./movies/movies.routers.js";
+import { setupSwagger } from "./swagger/swagger.js";
 
 const app = express();
 app.use(express.json());
+
+setupSwagger(app);
 
 app.use("/movies", moviesRoutes);
 
